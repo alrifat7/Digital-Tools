65f2631748680075d051f85a3af6b2bc86ddd0bc
Rifat <alrifat421@gmail.com>
2026-07-05 12:45:01 +0600

    Banner and card section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 import Navbar from "./Components/Layout/Navbar";
+import Header from "./Components/ui/Header";
+import Products from "./Components/Pages/Products";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const ProductsPromis = fetch("/ProductsCard.json").then((res) => res.json());
 
 function App() {
   const [selectCard, setSelectCard] = useState([]);
@@ -7,6 +13,13 @@ function App() {
   return (
     <>
       <Navbar SelectCard={selectCard} />
+      <Header />
+      <Products
+        ProductsPromis={ProductsPromis}
+        SelectCard={selectCard}
+        setSelectCard={setSelectCard}
+      />
+      <ToastContainer />
     </>
   );
 }
